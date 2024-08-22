e7983f9ecd0c4adb5e43bf3a710f4a7bb387bfe3
YoungJoon <93526791+youngjoon568@users.noreply.github.com>
2024-08-22 11:20:38 +0900

    💄header 디자인 개편

--- client/src/app/cart/layout.tsx
+++ client/src/app/cart/layout.tsx
@@ -2,7 +2,6 @@ import IconShared from "@/(FSD)/shareds/ui/IconShared";
 import LinkBtnShared from "@/(FSD)/shareds/ui/LinkBtnShared";
 import AppFixedBtmBar from "@/(FSD)/widgets/app/ui/AppFixedBtmBar";
 import AppFixedTopBar from "@/(FSD)/widgets/app/ui/AppFixedTopBar";
-import AppNav from "@/(FSD)/widgets/app/ui/AppNav";
 import AppTitleHeader from "@/(FSD)/widgets/app/ui/AppTitleHeader";
 import CartBtnBar from "@/(FSD)/widgets/cart/ui/CartBtnBar";
 import React from "react";
@@ -11,7 +10,7 @@ const Layout = ({ children, }: { children: React.ReactNode }) => {
     return (
         <>
             <AppFixedTopBar>
-                <AppTitleHeader title={"장바구니"} right={<LinkBtnShared href={"/search"} size={"sm"} isIconOnly endContent={<IconShared iconSize={"md"} iconType={"search"} />} />} />
+                <AppTitleHeader title={"장바구니"} buttons={<LinkBtnShared href={"/"} size={"sm"} isIconOnly endContent={<IconShared iconSize={"md"} iconType={"home"} />} />} />
             </AppFixedTopBar>
             {children}
             <AppFixedBtmBar>
