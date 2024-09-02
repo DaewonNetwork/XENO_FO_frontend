26b086fd98ab45717427d13fd53acea01bd8ed4f
YoungJoon <93526791+youngjoon568@users.noreply.github.com>
2024-09-02 17:47:32 +0900

    필요 없는 코드 제거

---
+++ client/src/(FSD)/shareds/stores/OrderProductAtom.ts
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const OrderProductAtom = atom<any>({
+    key: "OrderProductAtom",
+    default: [],
+});
--- client/src/app/products/[productId]/page.tsx
+++ client/src/app/products/[productId]/page.tsx
@@ -1,12 +1,10 @@
 import AppSection from "@/(FSD)/widgets/app/ui/AppSection";
-import ProductInfoContainer from "@/(FSD)/widgets/product/ui/ProductInfoContainer";
 import React from "react";
-
 
 const Page = () => {
     return (
         <AppSection isBgColor>
-            <ProductInfoContainer />
+            s
         </AppSection>
     );
 };
