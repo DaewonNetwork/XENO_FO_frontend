a0bb51bb744dd7fdff4286c05feca4fc3f64f76a
huntingforheat <asktt1234@gmail.com>
2024-09-09 09:38:26 +0900

    ✨ mainBanner 추가

--- client/src/(FSD)/widgets/home/ui/MainBanner.tsx
+++ client/src/(FSD)/widgets/home/ui/MainBanner.tsx
@@ -3,6 +3,11 @@
 import React from "react";
 import styles from "@/(FSD)/shareds/styles/MainStyle.module.scss";
 import Slider from "react-slick";
+import banner1 from "../../../../../../model/1.webp";
+import banner2 from "../../../../../../model/2.webp";
+import banner3 from "../../../../../../model/3.webp";
+import banner4 from "../../../../../../model/2426566_1_big.webp";
+
 
 const MainBanner = () => {
     const settings = {
@@ -19,7 +24,10 @@ const MainBanner = () => {
     return (
         <div className={`${styles.content} ${styles.banner}`}>
             <Slider {...settings}>
-                
+                <img src={banner1.src} className={styles.bannerImage} />
+                <img src={banner2.src} className={styles.bannerImage} />
+                <img src={banner3.src} className={styles.bannerImage} />
+                <img src={banner4.src} className={styles.bannerImage} />
             </Slider>
         </div>
     );
